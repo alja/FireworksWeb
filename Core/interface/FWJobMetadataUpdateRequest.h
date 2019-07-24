926e4337495915aecadc1a66d5dd7fa3ac57c43a
yuxiao <yuxiao.wang@cern.ch>
2019-07-24 13:30:50 -0700

    Add MetaDataJobManager

---
+++ Core/interface/FWJobMetadataUpdateRequest.h
@@ -0,0 +1,10 @@
+#ifndef Fireworks2_Core_FWJobMetadataUpdateRequest
+#define Fireworks2_Core_FWJobMetadataUpdateRequest
+
+class FWJobMetadataUpdateRequest
+{
+public:
+   virtual ~FWJobMetadataUpdateRequest() {};
+};
+
+#endif
