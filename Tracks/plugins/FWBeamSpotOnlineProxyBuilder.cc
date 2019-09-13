591060e25727c43ae30265dcd55903b1c7c99ab8
alja <amraktadel@ucsd.edu>
2019-09-13 12:36:09 -0700

    new beamspot proxy builder

---
+++ Tracks/plugins/FWBeamSpotOnlineProxyBuilder.cc
@@ -0,0 +1,98 @@
+/*
+ *  FWBeamSpotProxyBuilder.cc
+ *  FWorks
+ *
+ *  Created by Ianna Osborne on 7/29/10.
+ *
+ */
+
+#include "ROOT/REveDataSimpleProxyBuilderTemplate.hxx"
+#include "ROOT/REvePointSet.hxx"
+#include "ROOT/REveStraightLineSet.hxx"
+
+#include "DataFormats/Scalers/interface/BeamSpotOnline.h"
+#include "Fireworks2/Core/interface/FWEventItem.h"
+#include "Fireworks2/Core/interface/FWProxyBuilderFactory.h"
+
+
+using namespace ROOT::Experimental;
+
+class FWBeamSpotOnlineProxyBuilder : public REveDataSimpleProxyBuilderTemplate<BeamSpotOnline>
+{
+public:
+   REGISTER_FWPB_METHODS();
+
+private:
+   using REveDataSimpleProxyBuilderTemplate<BeamSpotOnline>::Build;
+   virtual void Build(const BeamSpotOnline& iData, int /*idx*/, REveElement* oItemHolder, const REveViewContext* vc) override
+   {
+      REvePointSet* pointSet = new REvePointSet;
+      SetupAddElement(pointSet, oItemHolder, true);
+
+      REveStraightLineSet* lineSet = new REveStraightLineSet;
+      SetupAddElement(lineSet, oItemHolder, true);
+
+      double posx = iData.x();
+      double posy = iData.y();
+      double posz = iData.z();
+      double errx = iData.err_x();
+      double erry = iData.err_y();
+      double errz = iData.err_z();
+
+      pointSet->SetNextPoint(posx, posy, posz);
+      pointSet->SetNextPoint(posx + errx, posy + erry, posz + errz);
+      pointSet->SetNextPoint(posx - errx, posy - erry, posz - errz);
+
+      lineSet->AddLine(posx + errx, posy + erry, posz + errz, posx - errx, posy - erry, posz - errz);
+   }
+};
+
+REGISTER_FW2PROXYBUILDER(FWBeamSpotOnlineProxyBuilder, BeamSpotOnline, "Beam Spot Online");
+
+/*
+class FWBeamSpotOnlineProxyBuilder : public FWSimpleProxyBuilderTemplate<BeamSpotOnline> {
+public:
+  FWBeamSpotOnlineProxyBuilder(void) {}
+  ~FWBeamSpotOnlineProxyBuilder(void) override {}
+
+  REGISTER_PROXYBUILDER_METHODS();
+
+private:
+  // Disable default copy constructor
+  FWBeamSpotOnlineProxyBuilder(const FWBeamSpotOnlineProxyBuilder&) = delete;
+  // Disable default assignment operator
+  const FWBeamSpotOnlineProxyBuilder& operator=(const FWBeamSpotOnlineProxyBuilder&) = delete;
+
+  using FWSimpleProxyBuilderTemplate<BeamSpotOnline>::build;
+  void build(const BeamSpotOnline& iData, unsigned int iIndex, REveElement& oItemHolder, const FWViewContext*) override;
+};
+
+void FWBeamSpotOnlineProxyBuilder::build(const BeamSpotOnline& iData,
+                                         unsigned int iIndex,
+                                         REveElement& oItemHolder,
+                                         const FWViewContext*) {
+  REvePointSet* pointSet = new REvePointSet;
+  setupAddElement(pointSet, &oItemHolder);
+
+  REveStraightLineSet* lineSet = new REveStraightLineSet;
+  setupAddElement(lineSet, &oItemHolder);
+
+  double posx = iData.x();
+  double posy = iData.y();
+  double posz = iData.z();
+  double errx = iData.err_x();
+  double erry = iData.err_y();
+  double errz = iData.err_z();
+
+  pointSet->SetNextPoint(posx, posy, posz);
+  pointSet->SetNextPoint(posx + errx, posy + erry, posz + errz);
+  pointSet->SetNextPoint(posx - errx, posy - erry, posz - errz);
+
+  lineSet->AddLine(posx + errx, posy + erry, posz + errz, posx - errx, posy - erry, posz - errz);
+}
+
+REGISTER_FWPROXYBUILDER(FWBeamSpotOnlineProxyBuilder,
+                        BeamSpotOnline,
+                        "Beam Spot Online",
+                        FWViewType::kAll3DBits | FWViewType::kAllRPZBits);
+*/
---
+++ Tracks/plugins/FWBeamSpotProxyBuilder.cc
@@ -0,0 +1,64 @@
+/*
+ *  FWBeamSpotProxyBuilder.cc
+ *  FWorks
+ *
+ *  Created by Ianna Osborne on 7/29/10.
+ *
+ */
+#include "ROOT/REveDataSimpleProxyBuilderTemplate.hxx"
+#include "ROOT/REveStraightLineSet.hxx"
+
+#include "DataFormats/BeamSpot/interface/BeamSpot.h"
+
+#include "Fireworks2/Core/interface/FWEventItem.h"
+#include "Fireworks2/Core/interface/FWProxyBuilderFactory.h"
+
+using namespace ROOT::Experimental;
+
+class FWBeamSpotProxyBuilder : public REveDataSimpleProxyBuilderTemplate<reco::BeamSpot>
+{
+public:
+   REGISTER_FWPB_METHODS();
+
+private:
+   using REveDataSimpleProxyBuilderTemplate<reco::BeamSpot>::Build;
+   virtual void Build(const reco::BeamSpot& bs, int /*idx*/, REveElement* oItemHolder, const REveViewContext* vc) override
+   {
+      REveStraightLineSet* ls = new REveStraightLineSet();
+
+      double pos[3] = {bs.x0(), bs.y0(), bs.z0()};
+      double e[3] = {bs.x0Error(), bs.y0Error(), bs.z0Error()};
+
+      const Int_t N = 32;
+      const Float_t S = 2 * TMath::Pi() / N;
+
+      Float_t a = e[0], b = e[1];
+      for (Int_t i = 0; i < N; i++)
+         ls->AddLine(
+                     a * TMath::Cos(i * S), b * TMath::Sin(i * S), 0, a * TMath::Cos(i * S + S), b * TMath::Sin(i * S + S), 0);
+
+      a = e[0];
+      b = e[2];
+      for (Int_t i = 0; i < N; i++)
+         ls->AddLine(
+                     a * TMath::Cos(i * S), 0, b * TMath::Sin(i * S), a * TMath::Cos(i * S + S), 0, b * TMath::Sin(i * S + S));
+
+      a = e[1];
+      b = e[2];
+      for (Int_t i = 0; i < N; i++)
+         ls->AddLine(
+                     0, a * TMath::Cos(i * S), b * TMath::Sin(i * S), 0, a * TMath::Cos(i * S + S), b * TMath::Sin(i * S + S));
+
+      ls->AddLine(0, 0, 0, 0, 0, 0);
+      ls->AddMarker(0, 0, 0);
+      ls->SetMarkerStyle(21);
+      //  const FWDisplayProperties& dp = FWProxyBuilderBase::item()->defaultDisplayProperties();
+      // ls->SetMarkerColor(dp.color());
+
+      ls->RefMainTrans().SetPos(pos);
+      SetupAddElement(ls, oItemHolder, true);
+   }
+};
+
+REGISTER_FW2PROXYBUILDER(FWBeamSpotProxyBuilder, reco::BeamSpot, "Beam Spot");
+
