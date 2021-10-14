704c377e2199111674ff4c2e05ef7ab8917218ff
alja <amraktadel@ucsd.edu>
2021-10-14 15:36:53 -0700

    Change client verison

--- Core/src/FW2Main.cc
+++ Core/src/FW2Main.cc
@@ -98,7 +98,7 @@ FW2Main::FW2Main():
       
    REveManager::Create();
    
-   ROOT::Experimental::gEve->GetWebWindow()->SetClientVersion("10.15");
+   ROOT::Experimental::gEve->GetWebWindow()->SetClientVersion("10.16");
 
    m_context->initEveElements();
    m_context->setGeom(&m_geom);
