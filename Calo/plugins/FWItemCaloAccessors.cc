66987160204da7c97e6b7ba79de326c000cc087c
Alja Mrak Tadel <amraktadel@ucsd.edu>
2022-04-08 15:50:00 -0700

    Remove test accessor

--- Calo/plugins/FWItemCaloAccessors.cc
+++ Calo/plugins/FWItemCaloAccessors.cc
@@ -4,16 +4,6 @@
 #include "FireworksWeb/Core/interface/FWItemRandomAccessor.h"
 #include "FireworksWeb/Core/interface/FWItemSingleAccessor.h"
 #include "FireworksWeb/Core/interface/FWProxyBuilderFactory.h"
-
-
-
-#include "SimDataFormats/Associations/interface/LayerClusterToCaloParticleAssociatorBaseImpl.h"
-class RecoToSimSingleAccessor : public FWItemSingleAccessor {
-public:
-  RecoToSimSingleAccessor(const TClass* x) : FWItemSingleAccessor(x) {}
-  ~RecoToSimSingleAccessor() override{};
-  REGISTER_FWITEMACCESSOR_METHODS();
-};
 /*
 REGISTER_TEMPLATE_FWITEMACCESSOR(FWItemDetSetAccessor<edm::DetSetVector<SiStripDigi> >,
                                  edm::DetSetVector<SiStripDigi>,
@@ -31,5 +21,4 @@ REGISTER_TEMPLATE_FWITEMACCESSOR(FWItemNewDetSetAccessor<edmNew::DetSetVector<Ph
                                  edmNew::DetSetVector<Phase2TrackerCluster1D>,
                                  "Phase2TrackerCluster1DCollectionNewAccessor");
 */
-REGISTER_FWITEMACCESSOR(RecoToSimSingleAccessor, hgcal::RecoToSimCollection, "RecoToSimAccessor");
 
