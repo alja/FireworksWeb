3de9a2c2c173477c0e37d8624b524af92c556971
Alja Mrak Tadel <amraktadel@ucsd.edu>
2022-04-11 11:02:50 -0700

    Add Purpose in registration

--- Core/interface/FWAssociationFactory.h
+++ Core/interface/FWAssociationFactory.h
@@ -11,8 +11,8 @@
 typedef edmplugin::PluginFactory<FWAssociationProxyBase*()> FWAssociationFactory;
 
 
-#define REGISTER_FWASSOCIATION(_builder_,_type_,_associationA_,_associationB_) \
-   DEFINE_FWASSOCIATION_METHODS(_builder_,_type_,_associationA_,_associationB_); \
-   DEFINE_EDM_PLUGIN(FWAssociationFactory, _builder_, _builder_::classRegisterTypeName() + "@" + _builder_::classAssociationA() + "@"+_builder_::classAssociationB() + "#" # _builder_)
+#define REGISTER_FWASSOCIATION(_builder_,_type_,_purpose_,_associationA_,_associationB_) \
+   DEFINE_FWASSOCIATION_METHODS(_builder_,_type_,_purpose_,_associationA_,_associationB_); \
+   DEFINE_EDM_PLUGIN(FWAssociationFactory, _builder_, _builder_::classRegisterTypeName() + "@" + _builder_::classPurpose()+ "#" # _builder_)
 
 #endif
