fcb8afb4430c4c576e1a6019e58a909e08c29310
Alja Mrak Tadel <amraktadel@ucsd.edu>
2022-10-10 16:16:32 -0700

    Don't show ecal barrel

--- Core/src/FWEveView.cc
+++ Core/src/FWEveView.cc
@@ -125,12 +125,12 @@ void FW3DView::importContext(ROOT::Experimental::REveViewContext *)
 {
   // Geometry
   auto b1 = new REveGeoShape("Barrel 1");
-  float dr = 3;
+  float dr = 1;
 
   fireworks::Context *ctx = fireworks::Context::getInstance();
   b1->SetShape(new TGeoTube(ctx->caloR1(), ctx->caloR2() + dr, ctx->caloZ1()));
-  b1->SetMainColor(kCyan);
-  m_geoScene->AddElement(b1);
+  b1->SetMainColor(kGray);
+ // m_geoScene->AddElement(b1);
 
   // calo
   REveCaloData *data = ctx->getCaloData();
@@ -139,7 +139,7 @@ void FW3DView::importContext(ROOT::Experimental::REveViewContext *)
 
   m_calo3d->SetBarrelRadius(ctx->caloR1(false));
   m_calo3d->SetEndCapPos(ctx->caloZ1(false));
-  m_calo3d->SetFrameTransparency(80);
+  m_calo3d->SetFrameTransparency(99);
   m_calo3d->SetAutoRange(false);
   m_calo3d->SetScaleAbs(true);
   m_calo3d->SetMaxTowerH(300);
