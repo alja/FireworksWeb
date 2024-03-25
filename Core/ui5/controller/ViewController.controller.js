45cc63bc957244ca43046f135171cd84387b2611
Alja Mrak Tadel <amraktadel@ucsd.edu>
2024-03-25 15:52:59 -0700

    Add option to print table to Log

--- Core/ui5/controller/ViewController.controller.js
+++ Core/ui5/controller/ViewController.controller.js
@@ -35,9 +35,41 @@ sap.ui.define([
             */
            this.mgr = this.getView().getViewData().mgr;
            this.eveView = this.getView().getViewData().eveView;
+           this.locateEveTable();
             this.byId("viewController").open();
-        }
 
+
+
+            
+        },
+
+        locateEveTable: function () {
+         this.eveTable = 0;
+         let element = this.eveView;//this.mgr.GetElement(this.eveViewerId);
+         let sceneInfo = element.childs[0];
+         let scene = this.mgr.GetElement(sceneInfo.fSceneId);
+         // console.log(">>>table scene", scene);
+         if (scene.childs[0]._typename == "ROOT::Experimental::REveTableViewInfo") {
+            // presume table view manger is first child of table scene
+            this.viewInfo = scene.childs[0];
+         }
+
+         this.collection = this.mgr.GetElement(this.viewInfo.fDisplayedCollection);
+         // loop over products
+         for (let i = 1; i < scene.childs.length; ++i) {
+            let product = scene.childs[i];
+            if (product.childs && product.childs.length && product.childs[0].fCollectionId == this.viewInfo.fDisplayedCollection) {
+               // console.log("table found  ",product.childs[0] );
+               this.eveTable = product.childs[0];
+               break;
+            }
+         }
+      },
+
+      print: function() {
+        this.locateEveTable();
+        this.mgr.SendMIR("PrintTable()", this.eveTable.fElementId, "ROOT::Experimental::REveDataTable");
+      }
     });
 
     return ViewController;
